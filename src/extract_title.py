52f7f3c403c8885b279705a719f8472efc9cd172
jonathonrowe <jonathonlrowe@gmail.com>
2026-01-11 10:40:21 -0600

    created tests for extract title function

--- src/extract_title.py
+++ src/extract_title.py
@@ -1,5 +1,7 @@
 def extract_title(markdown):
     lines = markdown.strip().split("\n")
     for line in lines:
-        if line.startswith("# "):
+        if not line.startswith("# "):
+            raise Exception("No h1 header in file")
+        elif line.startswith("# "):
             return line[2:]
--- src/test_extract_title.py
+++ src/test_extract_title.py
@@ -1,3 +1,13 @@
 import unittest
 from extract_title import extract_title
 
+class TestExtractTitle(unittest.TestCase):
+    def test_extract_title_hello(self):
+        md = "# Hello"
+        extract = extract_title(md)
+        self.assertEqual(extract, "Hello")
+
+    def test_extract_title_exception(self):
+        md = "Hello"
+        with self.assertRaises(Exception):
+            extract_title(md)
