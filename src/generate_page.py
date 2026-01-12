d52f6c4c73e67ee3adeadacd1485a91914453a3d
jonathonrowe <jonathonlrowe@gmail.com>
2026-01-12 13:52:42 -0600

    refactored recursive page generation to use generate_page()

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,14 +4,17 @@ from markdown_to_html_node import markdown_to_html_node
 from htmlnode import *
 from extract_title import extract_title
 
-def markdown_to_html_string(markdown, template):
+def markdown_to_html_string(markdown, template, basepath):
     html_node = markdown_to_html_node(markdown)
     html_string = html_node.to_html()
     title = extract_title(markdown)
     new_html = template.replace("{{ Title }}", title)
-    return new_html.replace("{{ Content }}", html_string)
+    new_html = new_html.replace("{{ Content }}", html_string)
+    new_html = new_html.replace('href="/', f'href="{basepath}')
+    new_html = new_html.replace('src="/', f'src="{basepath}')
+    return new_html
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as from_file:
         from_md = from_file.read()
@@ -19,7 +22,7 @@ def generate_page(from_path, template_path, dest_path):
     with open(template_path, "r") as template_file:
         template = template_file.read()
 
-    new_html = markdown_to_html_string(from_md, template)
+    new_html = markdown_to_html_string(from_md, template, basepath)
 
     try:
         dest_dir = os.path.dirname(dest_path)
@@ -31,30 +34,20 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as dest_file:
         dest_file.write(new_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     print(f"Generating pages from {dir_path_content} to {dest_dir_path} using {template_path}")
-    with open(template_path, "r") as template_file:
-        template = template_file.read()
 
     entries = os.listdir(dir_path_content)
     for entry in entries:
         full_path = os.path.join(dir_path_content, entry)
-        if os.path.isfile(full_path):
-            with open(full_path, "r") as entry_file:
-                entry_md = entry_file.read()
-            new_html = markdown_to_html_string(entry_md, template)
-            
-            try:
-                os.makedirs(dest_dir_path, exist_ok=True)
-            except OSError as e:
-                print(f"Error creating directory {dest_dir_path}: {e}")
-            
-            base_name = os.path.basename(full_path)
-            md_file_path = Path(base_name)
-            html_name = md_file_path.with_suffix(".html")
+        if os.path.isfile(full_path) and full_path.endswith(".md"):
+            html_name = Path(entry).with_suffix(".html")
             final_path = os.path.join(dest_dir_path, html_name)
-            with open(final_path, "w") as dest_file:
-                dest_file.write(new_html)
-        else:
+            generate_page(full_path, template_path, final_path, basepath)
+
+        elif os.path.isdir(full_path):
             dest_full_path = os.path.join(dest_dir_path, entry)
-            generate_pages_recursive(full_path, template_path, dest_full_path)
+            generate_pages_recursive(full_path, template_path, dest_full_path, basepath)
+
+        else:
+            continue
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import sys
 from textnode import TextNode
 from copy_contents import copy_contents
-from generate_page import generate_pages_recursive
+from generate_page import generate_page, generate_pages_recursive
 
 def main():
     copy_contents("static", "public")
 
-    generate_pages_recursive("content", "template.html", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
+    generate_pages_recursive("content", "template.html", "public", basepath)
 
 if __name__ == "__main__":
     main()
