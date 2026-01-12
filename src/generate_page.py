654aff432bfc361d62bae3bb2b921d2e89d64e18
jonathonrowe <jonathonlrowe@gmail.com>
2026-01-12 08:27:56 -0600

    added new content for additional web pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -26,3 +26,6 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, "w") as dest_file:
         dest_file.write(new_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
