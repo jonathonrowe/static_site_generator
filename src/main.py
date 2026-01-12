12447c487c7b559937e13542cee19d35e79fa35a
jonathonrowe <jonathonlrowe@gmail.com>
2026-01-12 14:30:47 -0600

    created build.sh and added new docs folder with appropriate files

--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ from copy_contents import copy_contents
 from generate_page import generate_page, generate_pages_recursive
 
 def main():
-    copy_contents("static", "public")
+    copy_contents("static", "docs")
+    
+    content_path = "content"
+    template_path = "template.html"
+    dest_dir_path = "docs"
 
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
     else:
         basepath = "/"
 
-    generate_pages_recursive("content", "template.html", "public", basepath)
+    generate_pages_recursive(content_path, template_path, dest_dir_path, basepath)
 
 if __name__ == "__main__":
     main()
